70cd9e3dd085e9fae109fb655cce02e3bf415ce4
behnamDehghannzhad <b.dehghannezhad.gitHub@gmail.com>
2021-09-03 18:03:50 +0330

    create multi-module project for data model. closes #1

--- src/test/java/spring/webapp/petclinic/PetClinicApplicationTests.java
+++ src/test/java/spring/webapp/petclinic/PetClinicApplicationTests.java
@@ -1,13 +1,13 @@
 package spring.webapp.petclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
