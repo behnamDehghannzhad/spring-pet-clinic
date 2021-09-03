1d12180dabeb4c993a47fe555598ead0935cd026
behnamDehghannzhad <b.dehghannezhad.gitHub@gmail.com>
2021-09-03 22:37:38 +0330

    Add id to model Objects. closes #10

---
+++ pet-clinic-data/src/main/java/spring/webapp/petclinic/model/BaseEntity.java
@@ -0,0 +1,14 @@
+package spring.webapp.petclinic.model;
+
+public class BaseEntity {
+
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
