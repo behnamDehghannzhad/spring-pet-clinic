3ec6259c2e7402743f11f3556292798605f71dbd
behnamDehghannzhad <b.dehghannezhad.gitHub@gmail.com>
2021-10-06 00:26:50 +0330

    implement map base services. closes #6

---
+++ pet-clinic-data/src/main/java/spring/webapp/petclinic/servies/map/OwnerMapService.java
@@ -0,0 +1,34 @@
+package spring.webapp.petclinic.servies.map;
+
+import spring.webapp.petclinic.model.Owner;
+import spring.webapp.petclinic.servies.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner>findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
