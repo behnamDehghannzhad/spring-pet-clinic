36344cdefad3842a0dffee0868b8845522425f8b
behnamDehghannzhad <b.dehghannezhad.gitHub@gmail.com>
2021-10-10 20:42:24 +0330

    Create Visit Map Service and Spring Data JPA service. closes #38

---
+++ pet-clinic-data/src/main/java/spring/webapp/petclinic/servies/VisitService.java
@@ -0,0 +1,6 @@
+package spring.webapp.petclinic.servies;
+
+import spring.webapp.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/spring/webapp/petclinic/servies/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package spring.webapp.petclinic.servies.map;
+
+import org.springframework.stereotype.Service;
+import spring.webapp.petclinic.model.Visit;
+import spring.webapp.petclinic.servies.VisitService;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Service
+public class VisitMapService  extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        boolean isInValidPet = Objects.isNull(visit.getPet()) || Objects.isNull(visit.getPet().getId());
+        boolean isInValidOwner = Objects.isNull(visit.getPet().getOwner()) || Objects.isNull(visit.getPet().getOwner().getId());
+        if(isInValidPet || isInValidOwner)
+            throw new RuntimeException("invalid visit");
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
